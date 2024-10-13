357c7d357df89963bd7b6555b965d21a4f089975
leonardorafaelli <leonardorafaelli@gmail.com>
2024-10-13 16:22:14 +0200

    feat: implements footer component

--- airbnb-clone-front/src/app/app.component.ts
+++ airbnb-clone-front/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
 import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent, FooterComponent, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
