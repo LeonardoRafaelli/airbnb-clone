c287350ff83f67d9db4425833f449af802ffeecd
leonardorafaelli <leonardorafaelli@gmail.com>
2024-10-16 23:31:04 +0200

    feat: toast with observable, ready to use

--- airbnb-clone-front/src/app/app.component.ts
+++ airbnb-clone-front/src/app/app.component.ts
@@ -1,30 +1,55 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
 
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
-import { NavbarComponent } from "./layout/navbar/navbar.component";
-import { FooterComponent } from "./layout/footer/footer.component";
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ToastService } from './layout/toast.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule, NavbarComponent, FooterComponent, ToastModule],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    FontAwesomeModule,
+    NavbarComponent,
+    FooterComponent,
+    ToastModule,
+  ],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit{
-
+export class AppComponent implements OnInit {
   faIconLibrary = inject(FaIconLibrary);
   isListingView = true;
+  toastService = inject(ToastService);
+  messageService = inject(MessageService)
 
   ngOnInit(): void {
     this.initFontAwesome();
+    this.listenToastService();
   }
 
   private initFontAwesome(): void {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  private listenToastService(): void {
+    this.toastService.sendSub.subscribe({
+      next: (newMessage) => {
+        if (newMessage && newMessage.summary !== this.toastService.INIT_STATE) {
+          this.messageService.add(newMessage);
+        }
+      },
+    });
   }
 }
--- airbnb-clone-front/src/app/layout/navbar/navbar.component.ts
+++ airbnb-clone-front/src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -7,6 +7,7 @@ import { CategoryComponent } from './category/category.component';
 import { AvatarComponent } from './avatar/avatar.component';
 import { DialogService } from "primeng/dynamicdialog";
 import { MenuItem } from 'primeng/api';
+import { ToastService } from '../toast.service';
 
 @Component({
   selector: 'app-navbar',
@@ -29,13 +30,16 @@ export class NavbarComponent implements OnInit {
   guests = "Add Guests";
   dates = "Any week";
 
+  toastService = inject(ToastService);
+
   // login () => this.authService.login();
   // login () => this.authService.logout();
 
   currentMenuItems: MenuItem[] | undefined = [];
 
   ngOnInit(): void {
-
+    this.currentMenuItems = this.fetchMenu();
+    this.toastService.send({severity: "info", summary: "Welcome to your Angular App"})
   }
 
   private fetchMenu() {
