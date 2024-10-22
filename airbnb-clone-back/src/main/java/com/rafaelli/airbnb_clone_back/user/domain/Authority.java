e72ae71eab55c5345a996995d6d7d7d85e2af080
leonardorafaelli <leonardorafaelli@gmail.com>
2024-10-22 23:51:45 +0200

    feat: adds Authority Serializable class

---
+++ airbnb-clone-back/src/main/java/com/rafaelli/airbnb_clone_back/user/domain/Authority.java
@@ -0,0 +1,12 @@
+package com.rafaelli.airbnb_clone_back.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "authority")
+public class Authority implements Serializable {
+
+}
